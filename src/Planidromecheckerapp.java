4413defd6787c1be1ae228b2942cc616ee8e5c0c
shahianurag7 <ak3461@srmist.edu.in>
2026-02-24 11:02:27 +0530

    UC4:Hardcoded palindrome

--- src/Planidromecheckerapp.java
+++ src/Planidromecheckerapp.java
@@ -1,18 +1,22 @@
 public class Planidromecheckerapp {
 
     public static void main(String[] args) {
-        String input = "madam";
-        String reversed = "";
+        String input = "radar";
+        char[] chars = input.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
         boolean isPalindrome = true;
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+        while (start < end) {
+            if ( chars[start]!=chars[end]){
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
         }
-        boolean isPlaindrome = input.equals(reversed);
-        System.out.println("Input text:" + input);
-        System.out.println("Reversed text:" + reversed);
-        System.out.println("Is is a palindrome?: " + isPalindrome);
+        System.out.println("Input:" + input);
+        System.out.println("Is Palindrome?:" + isPalindrome);
     }
 }
-
 
 

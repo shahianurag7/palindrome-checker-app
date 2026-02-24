81b7b928c54f75b45e91225278b3689e46c68a64
shahianurag7 <ak3461@srmist.edu.in>
2026-02-24 09:58:41 +0530

    UC2:Hardcoded palindrome

--- src/Planidromecheckerapp.java
+++ src/Planidromecheckerapp.java
@@ -1,8 +1,17 @@
 public class Planidromecheckerapp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to plaindrome checker management app");
-        System.out.println("Version : 1.0");
-                System.out.println("System initialized successfully");
+        String input = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text :" + input);
+        System.out.println(" Is it a Plaindrome " +isPalindrome);
     }
 }
+
+
